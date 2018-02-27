c570ec546d23cce51df78274feb83626eb24c5c7
hector ariceaga <hector_ariceaga@hotmail.com>
2018-02-27 16:48:25 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -4,11 +4,11 @@ function forLoop(array){
 for (let i = 0; i < 25; i++){
   if (i === 0){
     array.push(`I am 1 a strange loop.`)
-    i++
+    i =+ 1
   }
   else{
     array.push(`I am ${i} strange loops.`)
-    i++
+    i =+ 1
   }
   }
   return array;
