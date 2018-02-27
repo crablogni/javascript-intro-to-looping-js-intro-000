417b01fc85dc989b2b08067674e4452229a15cbd
hector ariceaga <hector_ariceaga@hotmail.com>
2018-02-27 17:07:48 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -20,3 +20,13 @@ function whileLoop(n){
     }
   }
 }
+
+function doWhileLoop(array){
+  function maybeTrue() {
+    return Math.random() >= 0.5;
+  }
+  do {
+    array.shift();
+  } while (array.length >0 && maybeTrue());
+  return array;
+}
